c4c6297835aae11e6743ddec48f67de6245ad13f
Eduard Vercaemer <vercaemereduard@gmail.com>
2021-05-05 21:36:10 -0500

    Heavily improve codegen clarity

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod parse;
 use parse::{Args, Arg};
 
 #[proc_macro_attribute]
-pub fn fmt(attr: TokenStream, item: TokenStream) -> TokenStream {    
+pub fn fmt(attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(item as syn::ItemStruct);
     let args = syn::parse_macro_input!(attr as Args);
 
@@ -19,12 +19,12 @@ pub fn fmt(attr: TokenStream, item: TokenStream) -> TokenStream {
     let s_fields = struct_field_idents(s);   // struct field idents
     let s_parse = argument_parsing(args);   // code that parses the struct
 
-    let tokens = quote! {
+    let code = quote! {
         #s
 
         impl ::std::str::FromStr for #s_ident {
-            type Err = ();
-            fn from_str(s: &str) -> Result<#s_ident, <#s_ident as ::std::str::FromStr>::Err> {
+            type Err = String;
+            fn from_str(s: &str) -> Result<#s_ident, String> {
                 #s_parse
 
                 Ok(#s_ident {
@@ -34,7 +34,7 @@ pub fn fmt(attr: TokenStream, item: TokenStream) -> TokenStream {
         }
     };
 
-    tokens.into()
+    code.into()
 }
 
 /// From a struct item, get the vector of field idents
@@ -57,47 +57,22 @@ fn argument_parsing(args: Args) -> TokenStream2 {
 
     while let Some(arg) = args.next() {
         match arg {
-            // consume text
             Arg::Text(lit) => {
-                let code = quote! {
-                    if !s.starts_with(#lit) {
-                        return Err(());
-                    }
-                    let (_,s) = s.split_at(#lit .as_bytes().len());
-                };
-                parse.push(code);
+                parse.push(parse_lit_str(lit));
             }
 
-            // capture value
             Arg::Ident(ident) => {
-                let code = if let Some(next) = args.next() {
-                    // more elements
-                    let lit = match next {
-                        Arg::Ident(_) => panic!("bad arg"),
+                if let Some(next) = args.next() {
+                    // capture with lookahead
+                    let lookahead = match next {
+                        Arg::Ident(_) => panic!("bad format string"),
                         Arg::Text(lit) => lit,
                     };
-                    quote! {
-                        let idx = match s.find(#lit) {
-                            Some(idx) => idx,
-                            _ => return Err(()),
-                        };
-                        let (left, s) = s.split_at(idx);
-                        let (_, s) = s.split_at(#lit .as_bytes().len());
-                        let #ident = match left.parse() {
-                            Ok(value) => value,
-                            _ => return Err(()),
-                        };
-                    }
+                    parse.push(parse_capture_w_lookahead(ident, lookahead));
                 } else {
-                    // final element
-                    quote! {
-                        let #ident = match s.parse() {
-                            Ok(value) => value,
-                            _ => return Err(()),
-                        };
-                    }
-                };
-                parse.push(code);
+                    // capture w/o lookahead
+                    parse.push(parse_capture(ident));
+                }
             }
         };
     }
@@ -105,4 +80,81 @@ fn argument_parsing(args: Args) -> TokenStream2 {
     quote! {
         #(#parse)*
     }    
+}
+
+/// Codegen for a literal string argument
+///
+/// The code generated makes sure the string we are parsing matches the
+/// string literal, in which case it consumes it and continues, otherwise
+/// it returns a parse error.
+///
+/// Example --
+///
+/// CODEGEN ARGS:
+///   lit_str = "abc"
+/// SCOPE IN:
+///   s = "abcxyz"
+/// SCOPE OUT:
+///   s = "xyz"
+fn parse_lit_str(lit_str: syn::LitStr) -> TokenStream2 {
+    quote! {
+        if !s.starts_with(#lit_str) {
+            return Err("parse_lit_str".to_owned());
+        }
+        
+        let (_, s) = s.split_at(#lit_str .as_bytes().len());
+    }
+}
+
+/// Codegen for field member capture
+///
+/// The code generated attempts to parse a field value from the whole remainings
+/// of the string in scope.
+///
+/// Example --
+///
+/// CODEGEN ARGS:
+///   capture = 'v' (an integer)
+/// SCOPE IN:
+///   s = "65"
+/// SCOPE OUT:
+///   s = "65"
+///   v = 65
+fn parse_capture(capture: syn::Ident) -> TokenStream2 {
+    quote! {
+        let #capture = match s.parse() {
+            Ok(v) => v,
+            Err(e) => return Err(format!("{}", e)),
+        };
+    }
+}
+
+/// Codegen for field member capture with lookahead
+///
+/// The code generated makes sure a certain string literal lookahead is valid,
+/// then captures the string before it into a capture field.
+///
+/// Example --
+///
+/// CODEGEN ARGS:
+///   capture = 'v' (an integer)
+///   lookahead = "abc"
+/// SCOPE IN:
+///   s = "65abcxyz"
+/// SCOPE OUT:
+///   s = "xyz"
+///   v = 65
+fn parse_capture_w_lookahead(capture: syn::Ident, lookahead: syn::LitStr) -> TokenStream2 {
+    quote! {
+        let idx = match s.find(#lookahead) {
+            Some(idx) => idx,
+            None => return Err(format!("lookahead for \"{}\" not matched", #lookahead)),
+        };
+        let (left, s) = s.split_at(idx);
+        let (_, s) = s.split_at(#lookahead .as_bytes().len());
+        let #capture = match left.parse() {
+            Ok(value) => value,
+            Err(e) => return Err(format!("{}", e)),
+        };
+    }
 }
